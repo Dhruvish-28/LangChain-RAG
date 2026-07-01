2702f1803887e75f369a275c8b70c51aaafb05a4
Dhruvish-28 <dhruvishparikh28@gmail.com>
2026-07-01 21:12:19 +0530

    Integrated Gemini model\

---
+++ retrieval/prompt_response.py
@@ -0,0 +1,51 @@
+from langchain_core.prompts import ChatPromptTemplate
+from .retriever import retrieve
+from .llm_model import llm
+
+def prompt_template(question):
+
+    docs = retrieve(question)
+
+    context = "\n\n".join( doc.page_content for doc in docs )
+
+    template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+You are a helpful AI assistant.
+
+Answer ONLY from the provided context.
+
+If the answer is not present in the context,
+reply:
+
+"I couldn't find that information in the uploaded documents."
+
+Keep answers concise.
+"""
+        ),
+
+        (
+            "human",
+            """
+Context:
+{context}
+
+Question:
+{question}
+"""
+        )
+    ]
+)
+
+    prompt = template.invoke(
+    {
+        "context": context,
+        "question": question
+    }
+)
+
+    response = llm.invoke(prompt)
+
+    return response
