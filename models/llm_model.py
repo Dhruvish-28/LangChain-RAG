cb987f6781978f4ed485fc9a4e209ae9fa3008d2
Dhruvish Parikh <dhruvishparikh28@gmail.com>
2026-08-06 16:40:09 +0530

    Refactor LLM model initialization with caching

--- models/llm_model.py
+++ models/llm_model.py
@@ -5,8 +5,12 @@ from pathlib import Path
 
 load_dotenv(Path(__file__).resolve().parent.parent / ".env")
 
-llm = ChatGoogleGenerativeAI(
+@st.cache_resource
+def load_gemini_client():
+    return  ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     google_api_key = os.getenv("GEMINI_API_KEY"),
     temperature=0
 )
+
+llm = load_gemini_client()
